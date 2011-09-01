9ea89d18fd59f4ccd722ab5733d0e7aca1582fbd
Lincoln Stoll <lstoll@lstoll.net>
2011-08-31 21:57:56 -0700

    Set port from env var if set.

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -3,7 +3,9 @@ import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 
 object JettyLauncher {
   def main(args: Array[String]) {
-    val server = new Server(8080)
+    val port = (if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080)
+
+    val server = new Server(port)
     val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
 
     context.addFilter(classOf[CiderEndpointFilter], "/*", 0)
