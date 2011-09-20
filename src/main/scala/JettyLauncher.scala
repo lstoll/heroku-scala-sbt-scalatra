42a36de8c645590ef19930450ecdcee6727b687c
Lincoln Stoll <lstoll@lstoll.net>
2011-09-20 16:42:40 +1000

    less parens

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -3,7 +3,7 @@ import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 
 object JettyLauncher {
   def main(args: Array[String]) {
-    val port = (if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080)
+    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
 
     val server = new Server(port)
     val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
